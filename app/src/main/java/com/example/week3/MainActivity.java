8cb67d1ef9588e870906641cfb883c3931d406da
junseokho <hgsrun@gmail.com>
2024-04-18 20:59:53 +0900

    week4

--- app/src/main/java/com/example/week3/MainActivity.java
+++ app/src/main/java/com/example/week3/MainActivity.java
@@ -1,104 +1,41 @@
 package com.example.week3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.app.TabActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends AppCompatActivity {
-    EditText edit1,edit2;
-    Button btnAdd,btnSub,btnMul,btnDiv;
-    TextView textResult;
-    String num1,num2;
-    Integer result;
-    Button[] numButtons=new Button[10];
-    Integer[] numBtnIDs={R.id.BtnNum0,R.id.BtnNum1,R.id.BtnNum2,R.id.BtnNum3,R.id.BtnNum4,R.id.BtnNum5,R.id.BtnNum6,R.id.BtnNum7,R.id.BtnNum8,R.id.BtnNum9};
-    int i;
+import com.example.week3.R;
 
-    @SuppressLint("MissingInflatedId")
+public class MainActivity extends TabActivity {
+
+    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("테이블레이아웃 계산기");
-        edit1=(EditText) findViewById(R.id.Edit1);
-        edit2=(EditText) findViewById(R.id.Edit2);
-        btnAdd=(Button) findViewById(R.id.BtnAdd);
-        btnSub=(Button) findViewById(R.id.BtnSub);
-        btnMul=(Button) findViewById(R.id.BtnMul);
-        btnDiv=(Button) findViewById(R.id.BtnDiv);
-        textResult=(TextView) findViewById(R.id.TextResult);
-        btnAdd.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                num1=edit1.getText().toString();
-                num2=edit2.getText().toString();
-                result=Integer.parseInt(num1)+Integer.parseInt(num2);
-                textResult.setText("계산 결과 : "+result.toString());
-                return false;
-            }
-        });
-        btnSub.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                num1=edit1.getText().toString();
-                num2=edit2.getText().toString();
-                result=Integer.parseInt(num1)-Integer.parseInt(num2);
-                textResult.setText("계산 결과 : "+result.toString());
-                return false;
-            }
-        });
-        btnMul.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                num1=edit1.getText().toString();
-                num2=edit2.getText().toString();
-                result=Integer.parseInt(num1)*Integer.parseInt(num2);
-                textResult.setText("계산 결과 : "+result.toString());
-                return false;
-            }
-        });
-        btnDiv.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                num1=edit1.getText().toString();
-                num2=edit2.getText().toString();
-                result=Integer.parseInt(num1)/Integer.parseInt(num2);
-                textResult.setText("계산 결과 : "+result.toString());
-                return false;
-            }
-        });
-        for (i=0;i<numBtnIDs.length;i++){
-            numButtons[i]=(Button) findViewById(numBtnIDs[i]);
-        }
-        for(i=0;i<numBtnIDs.length;i++){
-            final int index;
-            index=i;
+        TabHost tabHost = getTabHost();
 
-            numButtons[index].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        TabSpec tabSpec1 = tabHost.newTabSpec("TAG1").setIndicator("강아지");
+        tabSpec1.setContent(R.id.Dog);
+        tabHost.addTab(tabSpec1);
 
-                    if (edit1.isFocused()==true){
-                        num1=edit1.getText().toString()+numButtons[index].getText().toString();
-                        edit1.setText(num1);
-                    } else if (edit2.isFocused()==true) {
-                        num2=edit2.getText().toString()+numButtons[index].getText().toString();
-                        edit2.setText(num2);
-                    }else {
-                        Toast.makeText(getApplicationContext(),"먼저 에디트텍스트를 선택하세요",Toast.LENGTH_SHORT).show();
-                    }
+        TabSpec tabSpec2 = tabHost.newTabSpec("TAG2").setIndicator("고양이");
+        tabSpec2.setContent(R.id.Cat);
+        tabHost.addTab(tabSpec2);
 
-                }
+        TabSpec tabSpec3 = tabHost.newTabSpec("TAG3").setIndicator("토끼");
+        tabSpec3.setContent(R.id.Rabbit);
+        tabHost.addTab(tabSpec3);
 
-            });
-        }
+        TabSpec tabSpec4 = tabHost.newTabSpec("TAG4").setIndicator("말");
+        tabSpec4.setContent(R.id.Horse);
+        tabHost.addTab(tabSpec4);
+
+        tabHost.setCurrentTab(0);
+
     }
+
+
 }
